2cfd9b9b40f0148b826dbb345688a00b8557d669
straightchlrine <piotrlis555@gmail.com>
2022-02-27 22:22:07 +0100

    exasperated with myself; first exercise done

--- 16/it2016/praktyka/pi/src/PI.java
+++ 16/it2016/praktyka/pi/src/PI.java
@@ -3,12 +3,45 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class FirstExercise extends PI {
-
+	
+	private static boolean onCircle(Integer xCoord, Integer yCoord) {
+		boolean liesOnCircle = false;
+		
+		// r^2 = 40000
+		if((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) == 40000)
+			liesOnCircle = true;
+			
+		return liesOnCircle;
+	}
+	
+	private static boolean inCircle(Integer xCoord, Integer yCoord) {
+		boolean inside = false;
+		
+		if((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) < 40000)
+			inside = true;
+		
+		return inside;
+	}
+	
+	protected static void summary(ArrayList<Integer> xCoords, ArrayList<Integer> yCoords) {
+		int inside = 0;
+		System.out.println("--- Points on the circle ---");
+		for(int i = 0; i < xCoords.size(); i++) {
+			if(onCircle(xCoords.get(i), yCoords.get(i))) {
+				System.out.println("\t" + i + ": " + xCoords.get(i) + ", " + yCoords.get(i));
+				continue;
+			}
+			if(inCircle(xCoords.get(i), yCoords.get(i))) 
+				inside++;
+		}
+		
+		System.out.println("--- Points in the circle ---");
+		System.out.println("\t" + inside);
+	}
 }
 
 public class PI {
@@ -45,7 +78,7 @@ public class PI {
 		loadPoints(xCoords, yCoords);
 		
 		System.out.println("--- First Exercise ---");
-
+		FirstExercise.summary(xCoords, yCoords);
 	}
 
 }
