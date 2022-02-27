35afe0549c36ec64dcff7953449e4ac64a1888d6
straightchlrine <piotrlis555@gmail.com>
2022-02-27 22:54:11 +0100

    finished second exercise

--- 16/it2016/praktyka/pi/src/PI.java
+++ 16/it2016/praktyka/pi/src/PI.java
@@ -6,48 +6,95 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class FirstExercise extends PI {
-	
-	private static boolean onCircle(Integer xCoord, Integer yCoord) {
-		boolean liesOnCircle = false;
-		
-		// r^2 = 40000
-		if((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) == 40000)
-			liesOnCircle = true;
-			
-		return liesOnCircle;
-	}
+class SecondExercise extends PI {
 	
 	private static boolean inCircle(Integer xCoord, Integer yCoord) {
 		boolean inside = false;
-		
-		if((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) < 40000)
+
+		if ((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) <= 40000)
 			inside = true;
+
+		return inside;
+	}
+	
+	private static double getInside(ArrayList<Integer> xCoords, ArrayList<Integer> yCoords, int n) {
+		int inside = 0;
+			
+		for(int i = 0; i < n; i++) {
+			if(inCircle(xCoords.get(i), yCoords.get(i)))
+				inside++;
+		}
 		
 		return inside;
 	}
 	
+	private static double roundUpToFour(double toRound) {
+		return Math.round(toRound * 10000.0) / 10000.0;
+	}
+	
+	protected static void summary(ArrayList<Integer> xCoords, ArrayList<Integer> yCoords) {
+		double squareArea = Math.pow(400, 2);
+		double circleK = Math.pow(200, 2);
+	
+		System.out.println("--- Approximate value of PI for first 1000 ---");
+		double approximate = (squareArea * (getInside(xCoords, yCoords, 1000) / 1000)) / circleK;
+		System.out.println("\t" + roundUpToFour(approximate));
+		
+		System.out.println("--- Approximate value of PI for first 5000 ---");
+		approximate = (squareArea * (getInside(xCoords, yCoords, 5000) / 5000)) / circleK;
+		System.out.println("\t" + roundUpToFour(approximate));
+		
+		System.out.println("--- Approximate value of PI for all the data ---");
+		approximate = (squareArea * (getInside(xCoords, yCoords, 10000) / 10000)) / circleK;
+		System.out.println("\t" + roundUpToFour(approximate));
+	}
+}
+
+class FirstExercise extends PI {
+
+	protected static Integer inside = 0;
+
+	private static boolean onCircle(Integer xCoord, Integer yCoord) {
+		boolean liesOnCircle = false;
+
+		// r^2 = 40000
+		if ((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) == 40000)
+			liesOnCircle = true;
+
+		return liesOnCircle;
+	}
+
+	private static boolean inCircle(Integer xCoord, Integer yCoord) {
+		boolean inside = false;
+
+		if ((Math.pow(xCoord - 200, 2) + Math.pow(yCoord - 200, 2)) < 40000)
+			inside = true;
+
+		return inside;
+	}
+
 	protected static void summary(ArrayList<Integer> xCoords, ArrayList<Integer> yCoords) {
 		int inside = 0;
 		System.out.println("--- Points on the circle ---");
-		for(int i = 0; i < xCoords.size(); i++) {
-			if(onCircle(xCoords.get(i), yCoords.get(i))) {
-				System.out.println("\t" + i + ": " + xCoords.get(i) + ", " + yCoords.get(i));
+		for (int i = 0; i < xCoords.size(); i++) {
+			if (onCircle(xCoords.get(i), yCoords.get(i))) {
+				System.out.println("\t" + i + ": (" + xCoords.get(i) + ", " + yCoords.get(i) + ")");
 				continue;
 			}
-			if(inCircle(xCoords.get(i), yCoords.get(i))) 
+			if (inCircle(xCoords.get(i), yCoords.get(i)))
 				inside++;
 		}
-		
+
 		System.out.println("--- Points in the circle ---");
 		System.out.println("\t" + inside);
+		FirstExercise.inside = inside;
 	}
 }
 
 public class PI {
-	
+
 	private static void loadPoints(ArrayList<Integer> xCoords, ArrayList<Integer> yCoords) {
-		
+
 		Path path = Path.of("./resources", "punkty.txt");
 		try (BufferedReader bufferedReader = Files.newBufferedReader(path, Charset.forName("UTF-8"))) {
 			String line;
@@ -61,14 +108,14 @@ public class PI {
 				ArrayList<String> temp = new ArrayList<String>();
 				while (matcher.find())
 					temp.add(matcher.group());
-				
+
 				xCoords.add(Integer.parseInt(temp.get(0)));
 				yCoords.add(Integer.parseInt(temp.get(1)));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -76,9 +123,11 @@ public class PI {
 		ArrayList<Integer> yCoords = new ArrayList<Integer>();
 
 		loadPoints(xCoords, yCoords);
-		
-		System.out.println("--- First Exercise ---");
+
+		System.out.println("### First Exercise ###");
 		FirstExercise.summary(xCoords, yCoords);
+		System.out.println("### First Exercise ###");
+		SecondExercise.summary(xCoords, yCoords);
 	}
 
 }
