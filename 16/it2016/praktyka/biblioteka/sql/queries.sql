e38c87ac8d056260a28e08134663bb6a338111b9
straightchlrine <piotrlis555@gmail.com>
2022-02-28 23:12:25 +0100

    a bit of a problem with the last exercise, left for tomorrow

---
+++ 16/it2016/praktyka/biblioteka/sql/queries.sql
@@ -0,0 +1,55 @@
+use `exercise_library2016`;
+
+-- Zadanie 5.1
+select 
+	t.Student,
+	wypozyczenia.tytul as Tytul
+from (
+	select  
+		studenci.pesel as PESEL,
+		concat(studenci.imie, " ", studenci.nazwisko) as Student,
+		count(*) as Liczba
+	from studenci
+	join wypozyczenia on studenci.pesel = wypozyczenia.pesel
+	group by studenci.pesel
+	order by Liczba desc
+	limit 1) as t
+join wypozyczenia on t.PESEL = wypozyczenia.pesel;
+
+-- Zadanie 5.2
+select 
+	round(avg(t.Liczba), 4) as 'Średnia ilość studentów w jednym mieszkaniu'
+from (
+select 
+    count(*) as Liczba
+from meldunek
+join studenci on studenci.pesel = meldunek.pesel
+group by meldunek.id_pok) as t;
+
+-- Zadanie 5.3
+select 
+	'Ilość kobiet',
+    count(
+		if (cast(substr(studenci.pesel, 10, 1) as unsigned) % 2 = 0, 1, null) 
+    ) as 'Ilość'
+from studenci
+UNION
+select 
+	'Ilość mężczyzn',
+    count(
+		if (cast(substr(studenci.pesel, 10, 1) as unsigned) % 2 != 0, 1, null) 
+    )
+from studenci;
+
+-- Zadanie 5.4
+select 
+	concat(studenci.nazwisko, " ",studenci.imie) as Student
+from studenci
+where studenci.pesel not in (select meldunek.pesel from meldunek)
+order by Student asc;
+
+-- Zadanie 5.5
+
+
+
+
