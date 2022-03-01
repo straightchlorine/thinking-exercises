2fc40589bccfc0aac81e979f76edd9feedefe38e
straightchlrine <piotrlis555@gmail.com>
2022-03-01 01:01:46 +0100

    finished the exercise, need to review it tomorrow one way or another

--- 16/it2016/praktyka/biblioteka/sql/queries.sql
+++ 16/it2016/praktyka/biblioteka/sql/queries.sql
@@ -49,7 +49,29 @@ where studenci.pesel not in (select meldunek.pesel from meldunek)
 order by Student asc;
 
 -- Zadanie 5.5
+-- zwroc tytuly ksiazek z kazdego pokoju
 
 
+select 
+	sum(books) as 'Wypożyczone lektury'
+from (
+	-- number of books rented out by the people not living in the city
+	select 
+		count(wypozyczenia.tytul) as books
+	from studenci
+	join wypozyczenia on studenci.pesel = wypozyczenia.pesel
+	where studenci.pesel not in (select meldunek.pesel from meldunek)
+	union
+-- number of books rented out by the people living in the city(no duplcates)
+	select 
+		count(*)
+	from (
+		select distinct
+			meldunek.id_pok,
+			wypozyczenia.tytul
+		from wypozyczenia
+		join meldunek on wypozyczenia.pesel = meldunek.pesel    
+	) as t
+) as outerlayer
 
 
