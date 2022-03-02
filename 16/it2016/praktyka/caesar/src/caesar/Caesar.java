6a123f63f586fac4b5344d81f4fc373004ca8921
straightchlrine <piotrlis555@gmail.com>
2022-03-02 18:20:22 +0100

    finished second exercise; D =

--- 16/it2016/praktyka/caesar/src/caesar/Caesar.java
+++ 16/it2016/praktyka/caesar/src/caesar/Caesar.java
@@ -5,7 +5,17 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class SecondExercise extends Caesar {
+	
+	protected static String decipher(String cipher, int offset) {	
+
+		return FirstExercise.cipher(cipher, 26 - (offset % 26));
+	}
+	
+}
 
 class FirstExercise extends Caesar {
 
@@ -31,7 +41,9 @@ class FirstExercise extends Caesar {
 
 public class Caesar {
 	protected static ArrayList<String> sourceWords = new ArrayList<>();
-
+	protected static ArrayList<String> cipheredWords = new ArrayList<>();
+	protected static ArrayList<Integer> cipher = new ArrayList<>();
+	
 	private static void loadData() {
 
 		Path path = Path.of("./resources", "dane_6_1.txt");
@@ -49,12 +61,45 @@ public class Caesar {
 		}
 	}
 
+	private static void loadDataCipher() {
+		Path path = Path.of("./resources", "dane_6_2.txt");
+		
+		try (BufferedReader bufferedReader = Files.newBufferedReader(path, Charset.forName("UTF-8"))) {
+
+			String line = null;
+			Pattern pattern = Pattern.compile("\\S+");
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				Matcher matcher = pattern.matcher(line);
+				ArrayList<String> matches = new ArrayList<>();
+				
+				while(matcher.find()) 
+					matches.add(matcher.group(0));
+					
+				cipheredWords.add(matches.get(0));
+					
+				if(matches.size() == 1)
+					cipher.add(0);
+				else 
+					cipher.add(Integer.parseInt(matches.get(1)));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		loadData();
 		
-
 		for(int i = 0; i < sourceWords.size(); i++) {
-			System.out.println(FirstExercise.cipher(sourceWords.get(i), 107));
+			//System.out.println(FirstExercise.cipher(sourceWords.get(i), 107));
+		}
+			
+		loadDataCipher();
+		
+		for(int i = 0; i < cipheredWords.size(); i++) {
+			System.out.println(SecondExercise.decipher(cipheredWords.get(i), cipher.get(i)));
 		}
 		
 	}
