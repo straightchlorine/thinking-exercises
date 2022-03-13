c3cec5c829b4e90ea7cc139b9734f8d5177562eb
straightchlrine <piotrlis555@gmail.com>
2022-03-13 20:59:41 +0100

    slight change

--- 17/it2017/praktyka/sugar/sql/queries.sql
+++ 17/it2017/praktyka/sugar/sql/queries.sql
@@ -29,4 +29,24 @@ group by cennik.yearDate
 order by cennik.yearDate asc;
 
 -- Zadanie 4.4
--- need to wait a bit : /
+
+select
+	cukier.nip as NIP,
+	sum(cukier.tonnage * cennik.price) as total
+from cukier
+join cennik on year(cukier.dateID) = cennik.yearDate
+group by cukier.nip
+order by total desc;
+
+select
+	cukier.nip as NIP,
+    case
+		when sum(cukier.tonnage) >= 100 and sum(cukier.tonnage) < 1000 then sum(cukier.tonnage * (cennik.price - 0.05))
+        when sum(cukier.tonnage) >= 1000 and sum(cukier.tonnage) < 10000 then sum(cukier.tonnage * (cennik.price - 0.10))
+        when sum(cukier.tonnage) >= 10000 then sum(cukier.tonnage * (cennik.price - 0.20))
+        else sum(cukier.tonnage * cennik.price)
+	end as discounted_total
+from cukier
+join cennik on year(cukier.dateID) = cennik.yearDate
+group by cukier.nip
+order by discounted_total desc;
