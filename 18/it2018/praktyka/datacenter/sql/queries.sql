7d866f25914534e75e6ecddec2450e98300a4daa
straightchlrine <piotrlis555@gmail.com>
2022-03-31 17:48:20 +0200

    fixed, but should be simplified

--- 18/it2018/praktyka/datacenter/sql/queries.sql
+++ 18/it2018/praktyka/datacenter/sql/queries.sql
@@ -42,28 +42,39 @@ join naprawy on awarie.Numer_zgloszenia = naprawy.Numer_zgloszenia
 order by duration desc
 limit 1;
 
--- Zadanie 6.5(fix)
+-- Zadanie 6.5
 select
-	count(*)
+	c.fulfilling + d.fulfilling as totalClear
 from (
-
-select
-	komputery.Numer_komputera
-from komputery
-join awarie on komputery.Numer_komputera = awarie.Numer_komputera
-where awarie.Priorytet < 8
-
-union
-
-select
-	komputery.Numer_komputera
-from komputery
-where komputery.Numer_komputera not in (
+	(
 	select
-		Numer_komputera
-	from awarie
-)
-
-) as t;
-
-
+		count(case when b.fulfilled = 0 then 1 else null end) as fulfilling
+	from (
+		select 
+			t.id,
+			count(case when t.over8 > 0 then 1 else null end) as fulfilled
+			from (
+			select
+				komputery.Numer_komputera as id,
+				awarie.Priorytet as priorytet,
+				case
+					when awarie.Priorytet >= 8 then 1
+					else 0
+				end as over8
+			from komputery
+			join awarie on komputery.Numer_komputera = awarie.Numer_komputera
+			) as t
+		group by t.id
+	) as b
+) as c,
+(
+	select
+		count(*) as fulfilling
+	from komputery
+	where komputery.Numer_komputera not in (
+		select 
+			awarie.Numer_komputera
+		from awarie
+	)
+) as d
+);
